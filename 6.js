79db2425eb8824f41831c4ea816169fc295b8bb3
Zarina-2003 <107272179+Zarina-2003@users.noreply.github.com>
2022-06-16 14:38:47 +0300

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,39 @@
+let arr = ['alish', 'amir', 'amin', 'zarina', 'shoxrux ', 'kumush', 'amina', 'xoji', 'shox', 'ula', 'artyom'] 
+
+let name = prompt('what is ur name?') 
+if(name.toLocaleLowerCase().trim() === arr[0]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[1]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[2]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[3]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[4]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[5]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[6]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[7]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[8]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[9]){
+    alert('Welcome')
+}
+else if(name.toLocaleLowerCase().trim() === arr[10]){
+    alert('Welcome')
+}
+else{
+    alert('Git out')
+}
